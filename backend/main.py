317d64697e6ac80e5aa35a814d1533aca52b5eea
DushmanthaHerath1 <s23010560@ousl.lk>
2026-06-08 03:43:42 +0530

    feat(backend): create GET /api/menu endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+from typing import List
+
+import models
+import schemas
+from database import get_db
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
 app = FastAPI(title="Crumb & Crust API")
 
@@ -17,10 +23,9 @@ def read_root():
     return {"message": "Welcome to Crumb & Crust"}
 
 
-@app.get("/api/menu")
-def get_menu():
+@app.get("/api/menu", response_model=List[schemas.ProductResponse])
+def get_menu(db: Session = Depends(get_db)):
+    """Get all product data from database and send to front-end"""
 
-    return [
-        {"id": 1, "name": "Classic Sourdough", "price": 850.00},
-        {"id": 2, "name": "Chocolate Croissant", "price": 450.00},
-    ]
+    products = db.query(models.Product).filter(models.Product.is_active.is_(True)).all()
+    return products
